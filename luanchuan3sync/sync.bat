42b0d75fef281f2dc57bee51e0841e35b5289d83
longyangzz <longyangzz@126.com>
2019-12-05 16:28:46 +0800

    第一个栾川通过后版本

--- luanchuan3sync/sync.bat
+++ luanchuan3sync/sync.bat
@@ -1,1 +1,8 @@
+@echo off 
+
+　　if "%1" == "h" goto begin 
+
+　　mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+
+　　:begin 
 D:\数据同步程序\data-integration\kitchen /file:D:\数据同步程序\work\luanchuan3sync\sync3.kjb /level:Basic>>MySYNCTest.log
